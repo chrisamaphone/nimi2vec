6aa218d8ce370c5407727733f804d68ea248c0f6
Chris Martens <chrisamaphone@gmail.com>
2022-02-06 13:49:39 -0500

    updated preprocessing of corpus and training with skipgram

--- semantemusi.py
+++ semantemusi.py
@@ -5,18 +5,14 @@ import random
 from gensim.test.utils import datapath, get_tmpfile
 from gensim.models import KeyedVectors, Word2Vec
 
-path_glove = os.path.abspath('models/glove-vectors.txt');
-glove_file = datapath(path_glove);
+# glove model
+# model_path = os.path.abspath('models/glove-vectors.txt');
 
-model = KeyedVectors.load_word2vec_format(glove_file, binary=False, no_header=True);
+# w2v model
+model_path = os.path.abspath('models/w2v.txt');
 
-# Convert a value in the range (-1, 1)
-# to a "percent similarity" (0-100 range)
-def getPct(value):
-    return (value + 1)/2 * 100;
-
-def guess(guessed_word):
-    return getPct(model.similarity(guessed_word, secret_word));
+model_file = datapath(model_path);
+model = KeyedVectors.load_word2vec_format(model_file, binary=False);
 
 # switch to a different model file
 # noheader should be False if using w2v format, true if using GloVe
@@ -26,10 +22,17 @@ def switch_model(mname='w2v', noheader=False):
     model = KeyedVectors.load_word2vec_format(newfile, binary=False,
             no_header=noheader);
 
-switch_model(); # Switch to w2v instead of glove
-
 # Game stuff
 
+# Convert a value in the range (-1, 1)
+# to a "percent similarity" (0-100 range)
+def getPct(value):
+    return (value + 1)/2 * 100;
+
+def guess(guessed_word):
+    return getPct(model.similarity(guessed_word, secret_word));
+
+# Open wordlist
 possible_words = []
 lines = []
 with open('wordlist.txt') as f:
--- train-models.py
+++ train-models.py
@@ -2,30 +2,35 @@
 import gensim, logging, os
 from gensim.models import Word2Vec
 from gensim.test.utils import datapath
+from preproc import read_input
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 corpus_relpath = 'corpus/mdm-plus-pu.txt';
 corpus_path = os.path.abspath(corpus_relpath);
 
-sentences = []
-def read_input(path):
-    with open(path) as f:
-        for line in f:
-            # sentence = gensim.utils.simple_preprocess(line)
-            sentence = line.split()
-            sentences.append(sentence)
+### n.b.: replaced this with read_input fn in preproc to split out
+####  punctuation
+# sentences = []
+# def read_input(path):
+#     with open(path) as f:
+#         for line in f:
+#             # sentence = gensim.utils.simple_preprocess(line)
+#             sentence = line.split()
+#             sentences.append(sentence)
 
-read_input(corpus_path);
+sentences = read_input(corpus_path);
 
 logging.info('Done reading corpus file');
 
 # model = gensim.models.Word2Vec(corpus_file=datapath(corpus_path), vector_size=100, epochs=6);
 model = gensim.models.Word2Vec(
         sentences,
-        window=10,
-        min_count=2,
-        workers=10)
-model.train(sentences, total_examples=len(sentences), epochs=5)
+        vector_size=100,
+        window=5,
+        sg=1, # added: try skipgram model
+        min_count=2)
+        # , workers=10)
+model.train(sentences, total_examples=len(sentences), epochs=6)
 
 logging.info("Training complete, saving model")
 
